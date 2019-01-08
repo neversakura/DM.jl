aeb4273860e0525c3bc399b9e3f36cbf5cf62f1d
Sandy <neversakura@hotmail.com>
2019-01-08 09:58:28 -0800

    Allow task config json field to be null for folder, group and task_values

--- src/TaskManager.jl
+++ src/TaskManager.jl
@@ -7,7 +7,7 @@ Load task from task configuration file. Return a generator for all the `Task` de
 """
 function load_config_from_json(name)
     t = JSON.parsefile(name)
-    if haskey(t, "target_values")
+    if haskey(t, "target_values") && t["target_values"]!=nothing
         target_value_pair = parse_target_values(t["target_values"])
     else
         target_value_pair = ()
@@ -18,12 +18,12 @@ function load_config_from_json(name)
     else
         folder_path = ()
     end
-    if haskey(t, "group") && (t["group"])
+    if haskey(t, "group") && (t["group"]!=nothing)
         group_path = f(t["group"])
     else
         group_path = ()
     end
-    DataEntry(t["root"], folder_path, group_path)), create_task_list(target_value_pair...)
+    DataEntry(t["root"], folder_path, group_path), create_task_list(target_value_pair...)
 end
 
 """
