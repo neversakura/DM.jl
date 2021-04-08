1183213d5c550d7324075434cf23696ab015d40d
Huo Chen <neversakura@hotmail.com>
2021-04-07 23:18:59 -0700

    ⚗ add iterate function to a data_entry

--- src/DM.jl
+++ src/DM.jl
@@ -6,8 +6,9 @@ import CSV
 import JSON
 
 include("config.jl")
-include("data_entry.jl")
 include("utilities.jl")
+include("data_entry/base.jl")
+include("data_entry/iterate.jl")
 include("format_utilities/hdf5_util.jl")
 include("format_utilities/jld_util.jl")
 include("format_utilities/csv_util.jl")
@@ -17,6 +18,7 @@ export DataEntry
 export load_config_from_json, convert_recursive_array, parse_string_to_num
 export activate_param_set, get_param_set
 export save, load, check, load_file_array, delete, writeattr, readattr, read
+export walk_entry_file
 @reexport using CSVFiles, DataFrames
 
 end # end module
